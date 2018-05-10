2a5be4fdab86cb0d39d80e52b8d20301d93f2c0f
Antonio Raimundo <antonioraimundo007@gmail.com>
2018-05-09 23:22:44 -0300

    up up

--- src/main/java/br/ufc/vv/mycine/model/Sessao.java
+++ src/main/java/br/ufc/vv/mycine/model/Sessao.java
@@ -41,6 +41,8 @@ public class Sessao {
 	@NotBlank(message = "Fim da sessão é uma informação obrigatória")
 	private Date dataFim;
 	
+	
+	
 	public Long getId_secao() {
 		return id_secao;
 	}
@@ -77,5 +79,67 @@ public class Sessao {
 	public void setDataFim(Date dataFim) {
 		this.dataFim = dataFim;
 	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Sessao [id_secao=" + id_secao + ", filme=" + filme + ", sala=" + sala + ", horario=" + horario
+				+ ", dataInicio=" + dataInicio + ", dataFim=" + dataFim + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataFim == null) ? 0 : dataFim.hashCode());
+		result = prime * result + ((dataInicio == null) ? 0 : dataInicio.hashCode());
+		result = prime * result + ((filme == null) ? 0 : filme.hashCode());
+		result = prime * result + ((horario == null) ? 0 : horario.hashCode());
+		result = prime * result + ((id_secao == null) ? 0 : id_secao.hashCode());
+		result = prime * result + ((sala == null) ? 0 : sala.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sessao other = (Sessao) obj;
+		if (dataFim == null) {
+			if (other.dataFim != null)
+				return false;
+		} else if (!dataFim.equals(other.dataFim))
+			return false;
+		if (dataInicio == null) {
+			if (other.dataInicio != null)
+				return false;
+		} else if (!dataInicio.equals(other.dataInicio))
+			return false;
+		if (filme == null) {
+			if (other.filme != null)
+				return false;
+		} else if (!filme.equals(other.filme))
+			return false;
+		if (horario == null) {
+			if (other.horario != null)
+				return false;
+		} else if (!horario.equals(other.horario))
+			return false;
+		if (id_secao == null) {
+			if (other.id_secao != null)
+				return false;
+		} else if (!id_secao.equals(other.id_secao))
+			return false;
+		if (sala == null) {
+			if (other.sala != null)
+				return false;
+		} else if (!sala.equals(other.sala))
+			return false;
+		return true;
+	}
 
+	
 }
