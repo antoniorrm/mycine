5b3c1f21ea29ff6ae46c916e24d334d49ba97e90
Antonio Raimundo <antonioraimundo007@gmail.com>
2018-05-12 19:53:51 -0300

    #Repositoy add

--- src/main/java/br/ufc/vv/mycine/controller/SessaoController.java
+++ src/main/java/br/ufc/vv/mycine/controller/SessaoController.java
@@ -1,9 +1,13 @@
 package br.ufc.vv.mycine.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import br.ufc.vv.mycine.repository.SessaoRepository;
 
 @Controller 
 public class SessaoController {
 
-	
+	@Autowired
+	private SessaoRepository repository;
 }
